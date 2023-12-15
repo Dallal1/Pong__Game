b677921d9bb64401fa361eaddd9fda801f11ee75
Dallal1 <abw090571@gmail.com>
2023-12-16 00:52:29 +0100

    Ball

--- Pong/Pong/screen.cpp
+++ Pong/Pong/screen.cpp
@@ -3,53 +3,87 @@
 
 using namespace std;
 
+class Wall
+{
+public:
+	float pos_x, pos_y;
+	int speed_x, speed_y;
+
+	void Draw()
+	{
+		DrawRectangle(pos_x, pos_y, 15, 75, BLUE);
+	}
+
+	void Update()
+	{
+		if (IsKeyDown(KEY_D))
+			pos_x += speed_x;
+		else if (IsKeyDown(KEY_A))
+			pos_x -= speed_x;
+		else if (IsKeyDown(KEY_W))
+			pos_y -= speed_y;
+		else if (IsKeyDown(KEY_S))
+			pos_y += speed_y;
+	}
+
+};
+
+class Ball
+{
+public:
+	int radius;
+	float x, y;
+	int speed_x, speed_y;
+
+
+	void Draw()
+	{
+		DrawCircle(x, y, radius, GREEN);
+	}
+
+	void Update()
+	{
+		x = x + speed_x;
+		y = y + speed_y;
+	}
+};
+
+Ball ball;
+Wall wall;
+
 int main()
 {
-	float posX = 0;
-	float posY = 0;
-
 	//This IS Screen Size 
 	const int ScreenWidth = 800;
 	const int ScreenHigth = 450;
 	InitWindow(ScreenWidth, ScreenHigth, "Dallal Is King");
 
+	//Move The Bull
+	ball.radius = 10;
+	ball.x = ScreenWidth / 2;
+	ball.y = ScreenHigth / 2;
+	ball.speed_x = -2;
+	ball.speed_y = -2;
+
+	//Move The Wall
+	wall.pos_x = 5;
+	wall.pos_y = ScreenHigth / 2;
+	wall.speed_x = 2;
+	wall.speed_y = 2;
+
 	SetTargetFPS(144);
 
 	while (!WindowShouldClose())
 	{
-		if (IsKeyDown(KEY_D))
-		{
-			posX += GetFrameTime() * 200;
-		}
-		else if (IsKeyDown(KEY_A))
-		{
-			if (posX >= 0)
-				posX -= GetFrameTime() * 200;
-			else
-				posX;
-		}
-		else if (IsKeyDown(KEY_W))
-		{
-			if (posY <= 4)
-				posY;
-			else
-				posY -= GetFrameTime() * 200;
-		}
-		else if (IsKeyDown(KEY_S))
-		{
-			if (posY >= 390)
-				posY;
-			else
-				posY += GetFrameTime() * 200;
-		}
+		ball.Update();
+		wall.Update();
 
 		BeginDrawing();
 
 			ClearBackground(BLACK);
 			DrawLine(ScreenWidth / 2, 0, ScreenWidth / 2, ScreenHigth, RED); 
-			DrawRectangle(posX, posY, 8, 60, BLUE);
-			DrawCircle(ScreenWidth / 2, ScreenHigth / 2, 10, GREEN);
-			DrawRectangle(posX, posY, 8, 60, BLUE);
+			ball.Draw();
+			wall.Draw();
 		EndDrawing();
 	}
 
